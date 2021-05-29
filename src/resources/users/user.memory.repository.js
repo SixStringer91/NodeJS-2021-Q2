848183ef5e4d6f7ae50e498b18bc3854d1af5db8
SixStringer91 <danlootsev@gmail.com>
2021-05-30 02:01:02 +0300

    feat: add user docs

--- src/resources/users/user.memory.repository.js
+++ src/resources/users/user.memory.repository.js
@@ -1,13 +1,29 @@
 const USERS = [];
-
+ /**
+ * Get all users
+ * @returns {Array} returns array of users
+ */
 const getAll = async () => USERS;
+ /**
+ * Create new user
+ * @param {Object} user user body array
+ * @returns {Object} returns created user
+ */
 const createNewUser = (user) => {
   USERS.push(user);
   return user;
 };
-
+ /**
+ * Get user by id
+ * @param {string} id user id
+ * @returns {Object} returns finded user
+ */
 const getOneUser = async (id) => USERS.find((user) => id === user.id);
-
+ /**
+ * Update user
+ * @param {Object} obj user body contains
+ * @returns {Object} returns finded user by id in obj
+ */
 const updateUser = (obj) => {
   const userIndex = USERS.findIndex((user) => obj.id === user.id);
   const currentUser = USERS[userIndex];
@@ -18,7 +34,11 @@ const updateUser = (obj) => {
   });
   return currentUser;
 };
-
+ /**
+ * Delete user
+ * @param {Object} id user id
+ * @returns {void}
+ */
 const deleteUser = async (id) =>
   USERS.splice(
     USERS.findIndex((user) => id === user.id),
