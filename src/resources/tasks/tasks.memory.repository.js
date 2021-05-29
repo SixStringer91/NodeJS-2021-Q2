361d01d7ca1c0e363c025452b1526f5b5871ddf2
SixStringer91 <danlootsev@gmail.com>
2021-05-30 00:29:13 +0300

    feat: add docs to task.memory

--- src/resources/tasks/tasks.memory.repository.js
+++ src/resources/tasks/tasks.memory.repository.js
@@ -1,15 +1,31 @@
 const TASKS = [];
-
+/**
+ * Get all tasks from fake db
+ * @param {number} string id of board
+ * @returns {object} task
+ */
 const getAll = async (boardsId) =>
   TASKS.filter((el) => el.boardId === boardsId);
-
+ /**
+ * Create new task
+ * @param {object} task task body
+ * @returns {object} generated task body
+ */ 
 const createTask = async (task) => {
   TASKS.push(task);
   return task;
 };
-
+ /**
+ * Get task by id
+ * @param {string} id task body
+ * @returns {object | undefined} finded task 
+ */ 
 const getTask = async (id) => TASKS.find((task) => task.id === id);
-
+ /**
+ * update task
+ * @param {obj} obj body of task
+ * @returns {object | null} finded task 
+ */ 
 const updateTask = async (obj) => {
   const taskIndex = TASKS.findIndex((user) => obj.id === user.id);
   if (taskIndex < 0) {
@@ -23,7 +39,11 @@ const updateTask = async (obj) => {
   });
   return currentTask;
 };
-
+ /**
+ * delete task
+ * @param {string} id if of task
+ * @returns {boolean} return true if task successfuly deleted, or false
+ */ 
 const deleteTask = async (id) => {
   for (let i = 0; i < TASKS.length; i += 1) {
     if (TASKS[i].id === id) {
@@ -33,7 +53,11 @@ const deleteTask = async (id) => {
   }
   return false;
 };
-
+ /**
+ * delete all tasks
+ * @param {string} id board id
+ * @returns {void} nothing
+ */ 
 const deleteAllTasks = async (boardId) => {
   TASKS.forEach((task, i) => {
     if (task.boardId === boardId) {
@@ -41,7 +65,11 @@ const deleteAllTasks = async (boardId) => {
     }
   });
 };
-
+ /**
+ * sets values ​​with remoted user to null
+ * @param {string} userId user id
+ * @returns {void} nothing
+ */ 
 const ifUserDeleted = async (userId) => {
   TASKS.forEach((task, i) => {
     if (task.userId === userId) {
