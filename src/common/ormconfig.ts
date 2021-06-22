0fc4c215eef07eb69d3661c2f12655ba6dae5fd7
SixStringer91 <danlootsev@gmail.com>
2021-06-22 23:39:04 +0300

    fix: fix images

--- src/common/ormconfig.ts
+++ src/common/ormconfig.ts
@@ -12,7 +12,7 @@ dotenv.config({
 const config = {
   type: 'postgres',
   synchronize: false,
-  host: process.env['DB_HOST'],
+  host: process.env['POSTGRES_HOST'],
   port: process.env['POSTGRES_PORT'],
   username: process.env['POSTGRES_USER'],
   password: process.env['POSTGRES_PASSWORD'],
